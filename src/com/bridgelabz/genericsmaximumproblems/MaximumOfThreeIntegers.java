bf8d459a4f4fe4f86c6b9a6d86ca97d25c104e08
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-04-06 16:46:00 +0530

    Add: Here we are finding the maximum of three float values

--- src/com/bridgelabz/genericsmaximumproblems/MaximumOfThreeIntegers.java
+++ src/com/bridgelabz/genericsmaximumproblems/MaximumOfThreeIntegers.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.genericsmaximumproblems;
 
 public class MaximumOfThreeIntegers {
-	public static Integer findMax(Integer a, Integer b, Integer c) {
-		Integer max = a;
+
+	public static Float findMax(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0) {
 			max = b;
 		}
@@ -13,18 +14,19 @@ public class MaximumOfThreeIntegers {
 	}
 
 	public static void main(String[] args) {
-
-		Integer a1 = 10, b1 = 5, c1 = 2;
-		Integer max1 = findMax(a1, b1, c1);
+		// Test case 1: max at 1st position
+		Float a1 = 10.5f, b1 = 5.8f, c1 = 2.7f;
+		Float max1 = findMax(a1, b1, c1);
 		System.out.println("Max of " + a1 + ", " + b1 + ", " + c1 + " is " + max1);
 
-		Integer a2 = 8, b2 = 20, c2 = 3;
-		Integer max2 = findMax(a2, b2, c2);
+		// Test case 2: max at 2nd position
+		Float a2 = 8.3f, b2 = 20.1f, c2 = 3.9f;
+		Float max2 = findMax(a2, b2, c2);
 		System.out.println("Max of " + a2 + ", " + b2 + ", " + c2 + " is " + max2);
 
-		Integer a3 = 7, b3 = 6, c3 = 9;
-		Integer max3 = findMax(a3, b3, c3);
+		// Test case 3: max at 3rd position
+		Float a3 = 7.2f, b3 = 6.1f, c3 = 9.8f;
+		Float max3 = findMax(a3, b3, c3);
 		System.out.println("Max of " + a3 + ", " + b3 + ", " + c3 + " is " + max3);
 	}
-
 }
